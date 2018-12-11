7e46c6bfc2d059a30c2455a30c86ba30a4b2a998
jidi1f <chernyshdanil106@gmail.com>
2018-12-11 21:43:07 +0300

    Работает, но без each

--- plaginSlider.js
+++ plaginSlider.js
@@ -1,68 +1,86 @@
 (function($){
   $.fn.slider = function(settings) {
-  	
-  	var options = $.extend({
-  		
 
-  		images: ' ',
-  		buttonNext: ' ',
+  	var options = $.extend({
+
+
+  	  	images: ' ',
+  		  buttonNext: ' ',
         buttonPrev: ' ',
         auto: false
-       
-        
+
+
   	},settings);
   	console.log(options);
-    
-     
-   return this.each(function(){
-  		
-         
-  		 this.images = $(options.images);
-  		
-         var i = 0;
+
+
+         var images = $(options.images);
+  		   var i = 0;
          var isRun = false;
-         console.log(this);
-        
-        this.prev = function() {
-         
-         alert("1");
-         if(isRun){
-         	return;
-         } 
-
-         isRun = true; 
-
-     options.images.eq(i).css({
-        	left: 0,
-        	top: 0
-        }).animate({
-      	width: 0
-      }, 500);
-       i--;
-     
-       if (i < 0) {
-        i = slider.images.length - 1;
-       }
-       
-      options.images.eq(i) .css({
-            bottom: 0,
-            right: 0,
-            top: 'auto',
-            left: 'auto' 
-        }).animate({
-      	width: '100%'
-      }, 500, function(){
-      	isRun = false;
-      });
-    };
 
 
-     document.querySelector(options.buttonPrev).onclick = slider.prev;
-    
-    
-  	})
-   
- 
+	      $(options.buttonPrev).on('click',function() {
+
+
+	         if(isRun){
+	         	return;
+	         }
+
+	         isRun = true;
+
+	         images.eq(i).css({
+	        	left: 0,
+	        	top: 0
+	        }).animate({
+	      	   width: 0
+	           }, 1000);
+	       i--;
+
+	       if (i < 0) {
+	         i = images.length - 1;
+	       }
+
+	      images.eq(i).css({
+	            bottom: 0,
+	            right: 0,
+	            top: 'auto',
+	            left: 'auto'
+	        }).animate({
+	      	      width: '100%'
+	           }, 1000, function(){
+	      	        isRun = false;
+	              });
+
+	    });
+
+      $(options.buttonNext).on('click',function() {
+            images.eq(i).css({
+               bottom: 0,
+               right: 0,
+               top: 'auto',
+               left: 'auto'
+             }).animate({
+      	           width: 0
+                }, 300);
+                i++;
+
+         if (i >= images.length) {
+             i = 0;
+         }
+          images.eq(i).css({
+              top: 0,
+              left: 0
+          })
+          .animate({
+      	    width: '100%'
+          }, 300);
+      })
+
+
+
+
+
+  return this;
 
   };
-})(jQuery); 
+})(jQuery);
